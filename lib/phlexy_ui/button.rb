e661e78450e2f404a997844d43bd638d6f3dd0c0
David Alejandro <15317732+davidalejandroaguilar@users.noreply.github.com>
2024-09-22 18:16:20 +0200

    Allow passing :modal option to Button
    It will allow opening a modal from a button click.

--- lib/phlexy_ui/button.rb
+++ lib/phlexy_ui/button.rb
@@ -2,9 +2,10 @@
 
 module PhlexyUI
   class Button < Base
-    def initialize(*, as: :button, **)
+    def initialize(*, as: :button, modal: nil, **)
       super(*, **)
       @as = as
+      @modal = modal
     end
 
     def view_template(&)
@@ -14,14 +15,60 @@ module PhlexyUI
         base_modifiers:,
         options:
       ).then do |classes|
+        if modal
+          # TODO: Remove this abomination once Phlex 2.0 is released.
+          #
+          # The cleanest way to do this is with a single:
+          #
+          # onclick: "#{modal}.showModal()"
+          #
+          # However, currently, Phlex does not allow you to use the "onclick"
+          # attribute.
+          #
+          # Once Phlex 2.0 is released, it will add a #safe method
+          # that will allow us to replace this with a single line:
+          #
+          # onclick: safe("#{modal}.showModal()")
+          #
+          # For now, at least this only runs once and uses event delegation
+          # so that it only adds a single event listener to the document.body.
+          #
+          # The downside is a bigger HTML payload.
+          options[:data] ||= {}
+          options[:data][:modal] = modal
+          script do
+            unsafe_raw <<~JS
+              (() => {
+                if (window.PhlexyUI && window.PhlexyUI.modalInitialized) {
+                  return;
+                }
+                
+                document.body.addEventListener("click", (event) => {
+                  if (event.target.tagName === 'BUTTON' && 
+                      event.target.dataset.modal) {
+                    const modal = document.getElementById(event.target.dataset.modal);
+                    if (modal) {
+                      modal.showModal();
+                    }
+                  }
+                });
+                
+                if (!window.PhlexyUI) window.PhlexyUI = {};
+                window.PhlexyUI.modalInitialized = true;
+              })();
+            JS
+          end
+        end
+
         public_send(as, class: classes, **options, &)
       end
     end
 
     private
 
+    attr_reader :modal
+
     register_modifiers(
-      # Modifiers
       # "sm:no-animation"
       # "md:no-animation"
       # "lg:no-animation"
