a6f38195ed337804f61f579e2b0f3b27fc13f9af
David Alejandro <15317732+davidalejandroaguilar@users.noreply.github.com>
2024-09-29 22:47:20 +0200

    Add modifiers to navbar

--- spec/lib/phlexy_ui/navbar_spec.rb
+++ spec/lib/phlexy_ui/navbar_spec.rb
@@ -3,6 +3,53 @@ require "spec_helper"
 describe PhlexyUI::Navbar do
   subject(:output) { render described_class.new }
 
+  describe "modifiers" do
+    {
+      primary: "bg-primary text-primary-content",
+      secondary: "bg-secondary text-secondary-content",
+      accent: "bg-accent text-accent-content",
+      neutral: "bg-neutral text-neutral-content",
+      base_100: "bg-base-100 text-base-content",
+      base_200: "bg-base-200 text-base-content",
+      base_300: "bg-base-300 text-base-content",
+      info: "bg-info text-info-content",
+      success: "bg-success text-success-content",
+      warning: "bg-warning text-warning-content",
+      error: "bg-error text-error-content"
+    }.each do |condition, css|
+      context "when given :#{condition} condition" do
+        subject(:output) { render described_class.new(condition) }
+
+        it "renders it apart from the main class" do
+          expected_html = html <<~HTML
+            <nav class="navbar #{css}"></nav>
+          HTML
+
+          expect(output).to eq(expected_html)
+        end
+      end
+    end
+
+    context "when condition doesn't exist" do
+      it "does not raise an error" do
+        expect { render described_class.new(:foo) }
+          .not_to raise_error
+      end
+    end
+
+    context "when given multiple conditions" do
+      subject(:output) { render described_class.new(:primary, :secondary) }
+
+      it "renders them separately" do
+        expected_html = html <<~HTML
+          <nav class="navbar bg-primary text-primary-content bg-secondary text-secondary-content"></nav>
+        HTML
+
+        expect(output).to eq(expected_html)
+      end
+    end
+  end
+
   describe "rendering a full navbar" do
     let(:component) do
       Class.new(Phlex::HTML) do
