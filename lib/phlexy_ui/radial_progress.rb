b37e7674d10c7f84c753418257dd879fbda4e3f2
David Alejandro <15317732+davidalejandroaguilar@users.noreply.github.com>
2024-10-26 02:02:56 +0200

    Add outlined modifiers to RadialProgress

--- lib/phlexy_ui/radial_progress.rb
+++ lib/phlexy_ui/radial_progress.rb
@@ -50,50 +50,90 @@ module PhlexyUI
     attr_reader :value, :size, :thickness
 
     register_modifiers(
-      # "sm:bg-primary sm:text-primary-content"
-      # "md:bg-primary md:text-primary-content"
-      # "lg:bg-primary lg:text-primary-content"
-      primary: "bg-primary text-primary-content",
-      # "sm:bg-secondary sm:text-secondary-content"
-      # "md:bg-secondary md:text-secondary-content"
-      # "lg:bg-secondary lg:text-secondary-content"
-      secondary: "bg-secondary text-secondary-content",
-      # "sm:bg-accent sm:text-accent-content"
-      # "md:bg-accent md:text-accent-content"
-      # "lg:bg-accent lg:text-accent-content"
-      accent: "bg-accent text-accent-content",
-      # "sm:bg-neutral sm:text-neutral-content"
-      # "md:bg-neutral md:text-neutral-content"
-      # "lg:bg-neutral lg:text-neutral-content"
-      neutral: "bg-neutral text-neutral-content",
-      # "sm:bg-base-100 sm:text-base-content"
-      # "md:bg-base-100 md:text-base-content"
-      # "lg:bg-base-100 lg:text-base-content"
-      base_100: "bg-base-100 text-base-content",
-      # "sm:bg-base-200 sm:text-base-content"
-      # "md:bg-base-200 md:text-base-content"
-      # "lg:bg-base-200 lg:text-base-content"
-      base_200: "bg-base-200 text-base-content",
-      # "sm:bg-base-300 sm:text-base-content"
-      # "md:bg-base-300 md:text-base-content"
-      # "lg:bg-base-300 lg:text-base-content"
-      base_300: "bg-base-300 text-base-content",
-      # "sm:bg-info sm:text-info-content"
-      # "md:bg-info sm:text-info-content"
-      # "lg:bg-info sm:text-info-content"
-      info: "bg-info text-info-content",
-      # "sm:bg-success sm:text-success-content"
-      # "md:bg-success md:text-success-content"
-      # "lg:bg-success lg:text-success-content"
-      success: "bg-success text-success-content",
-      # "sm:bg-warning sm:text-warning-content"
-      # "md:bg-warning md:text-warning-content"
-      # "lg:bg-warning lg:text-warning-content"
-      warning: "bg-warning text-warning-content",
-      # "sm:bg-error sm:text-error-content"
-      # "md:bg-error md:text-error-content"
-      # "lg:bg-error lg:text-error-content"
-      error: "bg-error text-error-content"
+      # "sm:bg-primary sm:text-primary-content border-primary"
+      # "md:bg-primary md:text-primary-content border-primary"
+      # "lg:bg-primary lg:text-primary-content border-primary"
+      primary: "bg-primary text-primary-content border-primary",
+      # "sm:text-primary"
+      # "md:text-primary"
+      # "lg:text-primary"
+      primary_outline: "text-primary",
+      # "sm:bg-secondary sm:text-secondary-content border-secondary"
+      # "md:bg-secondary md:text-secondary-content border-secondary"
+      # "lg:bg-secondary lg:text-secondary-content border-secondary"
+      secondary: "bg-secondary text-secondary-content border-secondary",
+      # "sm:text-secondary"
+      # "md:text-secondary"
+      # "lg:text-secondary"
+      secondary_outline: "text-secondary",
+      # "sm:bg-accent sm:text-accent-content border-accent"
+      # "md:bg-accent md:text-accent-content border-accent"
+      # "lg:bg-accent lg:text-accent-content border-accent"
+      accent: "bg-accent text-accent-content border-accent",
+      # "sm:text-accent"
+      # "md:text-accent"
+      # "lg:text-accent"
+      accent_outline: "text-accent",
+      # "sm:bg-neutral sm:text-neutral-content border-neutral"
+      # "md:bg-neutral md:text-neutral-content border-neutral"
+      # "lg:bg-neutral lg:text-neutral-content border-neutral"
+      neutral: "bg-neutral text-neutral-content border-neutral",
+      # "sm:text-neutral"
+      # "md:text-neutral"
+      # "lg:text-neutral"
+      neutral_outline: "text-neutral",
+      # "sm:bg-base-100 sm:text-base-content border-base-100"
+      # "md:bg-base-100 md:text-base-content border-base-100"
+      # "lg:bg-base-100 lg:text-base-content border-base-100"
+      base_100: "bg-base-100 text-base-content border-base-100",
+      # "sm:text-base-100"
+      # "md:text-base-100"
+      # "lg:text-base-100"
+      base_100_outline: "text-base-100",
+      # "sm:bg-base-200 sm:text-base-content border-base-200"
+      # "md:bg-base-200 md:text-base-content border-base-200"
+      # "lg:bg-base-200 lg:text-base-content border-base-200"
+      base_200: "bg-base-200 text-base-content border-base-200",
+      # "sm:text-base-200"
+      # "md:text-base-200"
+      # "lg:text-base-200"
+      base_200_outline: "text-base-200",
+      # "sm:bg-base-300 sm:text-base-content border-base-300"
+      # "md:bg-base-300 md:text-base-content border-base-300"
+      # "lg:bg-base-300 lg:text-base-content border-base-300"
+      base_300: "bg-base-300 text-base-content border-base-300",
+      # "sm:text-base-300"
+      # "md:text-base-300"
+      # "lg:text-base-300"
+      base_300_outline: "text-base-300",
+      # "sm:bg-info sm:text-info-content border-info"
+      # "md:bg-info md:text-info-content border-info"
+      # "lg:bg-info lg:text-info-content border-info"
+      info: "bg-info text-info-content border-info",
+      # "sm:text-info"
+      # "md:text-info"
+      # "lg:text-info"
+      info_outline: "text-info",
+      # "sm:bg-success sm:text-success-content border-success"
+      # "md:bg-success md:text-success-content border-success"
+      # "lg:bg-success lg:text-success-content border-success"
+      success: "bg-success text-success-content border-success",
+      # "sm:text-success"
+      # "md:text-success"
+      # "lg:text-success"
+      success_outline: "text-success",
+      # "sm:bg-warning sm:text-warning-content border-warning"
+      # "md:bg-warning md:text-warning-content border-warning"
+      # "lg:bg-warning lg:text-warning-content border-warning"
+      warning: "bg-warning text-warning-content border-warning",
+      # "sm:text-warning"
+      # "md:text-warning"
+      # "lg:text-warning"
+      warning_outline: "text-warning",
+      # "sm:bg-error sm:text-error-content border-error"
+      # "md:bg-error md:text-error-content border-error"
+      # "lg:bg-error lg:text-error-content border-error"
+      error: "bg-error text-error-content border-error"
     )
   end
 end
