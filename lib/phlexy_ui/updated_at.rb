a15e03b50926fdeddfca829ecc2dce28bffe4713
David Alejandro <15317732+davidalejandroaguilar@users.noreply.github.com>
2025-03-30 13:42:04 -0700

    Release version 0.2.1

--- lib/phlexy_ui/updated_at.rb
+++ lib/phlexy_ui/updated_at.rb
@@ -3,5 +3,5 @@
 module PhlexyUI
   # This timestamp is automatically updated when releasing a new version
   # Format: YYYY-MM-DD HH:MM:SS UTC
-  UPDATED_AT = "2024-12-03 21:23:25 UTC"
+  UPDATED_AT = "2025-03-30 20:42:03 UTC"
 end
