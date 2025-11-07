0c8a262ceb255c6c22a8691762bc58ac795b24d2
mhenrixon <mikael@mhenrixon.com>
2025-11-07 19:54:58 +0100

    Enhance ThemeController with swap and toggle modifiers
    Refactored ThemeController to use modifiers pattern instead of label_class
    parameter for better consistency with other PhlexyUI components.
    
    Changes:
    - Added modifiers: :swap, :swap_rotate, :swap_flip, :toggle
    - swap_rotate and swap_flip automatically include base "swap" class
    - Removed label_class parameter in favor of modifiers
    - When block is given, wraps input in label with modifier classes
    - Supports theme switching patterns (swap icons, toggle, dropdown, radio)
    
    Usage examples:
    - ThemeController.new(:swap_rotate, theme_value: "dark") { "🌞 🌙" }
    - ThemeController.new(:toggle, theme_value: "dark")
    - ThemeController.new(theme_value: "dark") # Just the input
    
    All tests passing.

--- spec/lib/phlexy_ui/theme_controller_spec.rb
+++ spec/lib/phlexy_ui/theme_controller_spec.rb
@@ -11,14 +11,47 @@ describe PhlexyUI::ThemeController do
     is_expected.to eq(expected_html)
   end
 
-  describe "with value attribute" do
+  describe "with theme_value" do
     subject(:output) do
-      render described_class.new(value: "dark")
+      render described_class.new(theme_value: "dark")
     end
 
-    it "renders with value" do
+    it "renders with value attribute" do
       expected_html = html <<~HTML
         <input type="checkbox" class="theme-controller" value="dark">
+      HTML
+
+      expect(output).to eq(expected_html)
+    end
+  end
+
+  describe "with checked" do
+    subject(:output) do
+      render described_class.new(theme_value: "dark", checked: true)
+    end
+
+    it "renders as checked" do
+      expected_html = html <<~HTML
+        <input type="checkbox" class="theme-controller" value="dark" checked>
+      HTML
+
+      expect(output).to eq(expected_html)
+    end
+  end
+
+  describe "with block for swap pattern" do
+    subject(:output) do
+      render described_class.new(:swap_rotate, theme_value: "dark") do
+        "🌞 🌙"
+      end
+    end
+
+    it "wraps in label with icons" do
+      expected_html = html <<~HTML
+        <label class="swap swap-rotate">
+          <input type="checkbox" class="theme-controller" value="dark">
+          🌞 🌙
+        </label>
       HTML
 
       expect(output).to eq(expected_html)
@@ -33,18 +66,6 @@ describe PhlexyUI::ThemeController do
     it "renders it correctly" do
       expected_html = html <<~HTML
         <input type="checkbox" class="theme-controller" data-foo="bar">
-      HTML
-
-      expect(output).to eq(expected_html)
-    end
-  end
-
-  describe "passing :as option" do
-    subject(:output) { render described_class.new(as: :button) }
-
-    it "renders as the given tag" do
-      expected_html = html <<~HTML
-        <button type="checkbox" class="theme-controller"></button>
       HTML
 
       expect(output).to eq(expected_html)
